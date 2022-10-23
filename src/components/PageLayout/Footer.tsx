3af786131ba8d8a69439dce9194c8ccf3ef6f905
Scott Macdonald <scott.scotm@gmail.com>
2022-10-23 13:28:15 +0100

    Update link to github repo

--- src/components/PageLayout/Footer.tsx
+++ src/components/PageLayout/Footer.tsx
@@ -3,17 +3,17 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className="mt-4 flex flex-wrap">
-      <div className="lg:w-1/3 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+      <div className="border-l-2 border-gray-200 border-opacity-60 px-8 py-6 md:w-full lg:w-1/3">
+        <h2 className="title-font mb-2 text-lg font-medium text-gray-900 sm:text-xl">
           About this software
         </h2>
-        <p className="leading-relaxed text-base mb-4">
+        <p className="mb-4 text-base leading-relaxed">
           Latest source code can be{" "}
-          <Link href="https://github.com/scotm/lifting-toy-nextjs">
+          <Link href="https://github.com/scotm/lifting-t3">
             <a>found on GitHub.</a>
           </Link>
         </p>
-        <a className="text-indigo-500 inline-flex items-center">
+        <a className="inline-flex items-center text-indigo-500">
           Learn More
           <svg
             fill="none"
@@ -21,24 +21,24 @@ export default function Footer() {
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="2"
-            className="w-4 h-4 ml-2"
+            className="ml-2 h-4 w-4"
             viewBox="0 0 24 24"
           >
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
         </a>
       </div>
-      <div className="lg:w-1/3 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+      <div className="border-l-2 border-gray-200 border-opacity-60 px-8 py-6 md:w-full lg:w-1/3">
+        <h2 className="title-font mb-2 text-lg font-medium text-gray-900 sm:text-xl">
           Resistance Training
         </h2>
-        <p className="leading-relaxed text-base mb-4">
+        <p className="mb-4 text-base leading-relaxed">
           Regular resistance training can decrease the risk of heart disease by
           lowering body fat, decreasing blood pressure, improving cholesterol,
           and lowering the stress placed on the heart. Muscular fitness enhances
           your quality of life.
         </p>
-        <a className="text-indigo-500 inline-flex items-center">
+        <a className="inline-flex items-center text-indigo-500">
           Learn More
           <svg
             fill="none"
@@ -46,22 +46,22 @@ export default function Footer() {
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="2"
-            className="w-4 h-4 ml-2"
+            className="ml-2 h-4 w-4"
             viewBox="0 0 24 24"
           >
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
         </a>
       </div>
-      <div className="lg:w-1/3 md:w-full px-8 py-6 border-l-2 border-r-2 border-gray-200 border-opacity-60">
-        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+      <div className="border-l-2 border-r-2 border-gray-200 border-opacity-60 px-8 py-6 md:w-full lg:w-1/3">
+        <h2 className="title-font mb-2 text-lg font-medium text-gray-900 sm:text-xl">
           Tracking your gains
         </h2>
-        <p className="leading-relaxed text-base mb-4">
+        <p className="mb-4 text-base leading-relaxed">
           That which gets measured gets improved. Nothing motivates us more than
           seeing our progress.
         </p>
-        <a className="text-indigo-500 inline-flex items-center">
+        <a className="inline-flex items-center text-indigo-500">
           Learn More
           <svg
             fill="none"
@@ -69,7 +69,7 @@ export default function Footer() {
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="2"
-            className="w-4 h-4 ml-2"
+            className="ml-2 h-4 w-4"
             viewBox="0 0 24 24"
           >
             <path d="M5 12h14M12 5l7 7-7 7"></path>
